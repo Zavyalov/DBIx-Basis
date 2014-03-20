84352adbfebf3503827bebde71da2dce90157473
zavyalov <zavialov@corp.mail.ru>
2014-03-20 17:15:35 +0400

    New sql for tests

--- t/test.sql
+++ t/test.sql
@@ -1,4 +1,7 @@
+-- vim:nospell:
 DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS test2p;
+DROP TABLE IF EXISTS testnp;
 CREATE TABLE test (
     id          integer NOT NULL,
     value2      text,
@@ -9,3 +12,18 @@ CREATE TABLE test (
     PRIMARY KEY (id)
 );
 INSERT INTO test VALUES(1,NULL,NULL,'Test','{"value":"one"}');
+
+CREATE TABLE test2p (
+    key1        integer NOT NULL,
+    key2        integer NOT NULL,
+    val1        text,
+    data_basis  varchar NOT NULL,
+    data        blob,
+    --
+    PRIMARY KEY (key1,key2)
+);
+CREATE TABLE testnp (
+    val1        text,
+    data_basis  varchar NOT NULL,
+    data        blob
+);
